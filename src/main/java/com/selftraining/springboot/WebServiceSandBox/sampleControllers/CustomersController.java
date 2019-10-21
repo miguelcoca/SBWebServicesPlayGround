32e5102f41b043a38e9eefce077358a91b30e53d
Miguel Angel Coca <redridehell@gmail.com>
2019-10-21 16:26:37 -0400

    return not found status 404 with null object, insteat of ok 200 status and null body. /api/customers/customer/{id}

--- src/main/java/com/selftraining/springboot/WebServiceSandBox/sampleControllers/CustomersController.java
+++ src/main/java/com/selftraining/springboot/WebServiceSandBox/sampleControllers/CustomersController.java
@@ -6,8 +6,11 @@ import com.selftraining.springboot.WebServiceSandBox.Services.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -26,7 +29,13 @@ public class CustomersController {
     }
 
     @RequestMapping(value = "/customer/{id}")
-    public Optional<Customer> getCustomerWithID(@PathVariable(value = "id") String id){
-        return customersService.getCustomerWithID(id);
+    public ResponseEntity<?> getCustomerWithID(@PathVariable(value = "id") String id){
+        Optional<Customer> customerWithID = customersService.getCustomerWithID(id);
+
+        try{
+             return ResponseEntity.ok(customerWithID.get());
+         }catch (NoSuchElementException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 }
